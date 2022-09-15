1e5f664f3b46433a6618f5dc440d924808b57553
Apostrophe-s <113565929+Apostrophe-s@users.noreply.github.com>
2022-09-15 00:55:50 -0700

    Add files via upload

---
+++ name_age.py
@@ -0,0 +1,11 @@
+# This program says hello and asks for your name.
+print("Hello, world!")
+print("What is your name?")
+name = input()
+print("It is good to meet you, " + name)
+print("The length of your name is " + str(len(name)) + " letters long")
+print("What is your age?")
+age = input()
+print("You will be " + str(int(age) + 1) + " in a year.")
+print("Goodbye")
+
